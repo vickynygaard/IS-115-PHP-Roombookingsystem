5ec46da1c8fef088ad4ade2a53a99711da4a2664
Thea Nielsen <thea.nielsen@outlook.com>
2024-11-25 18:39:10 +0100

    endringer i admin

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -1,3 +1,5 @@
+
+
 <div class="container-fluid bg-dark text-light p-3 d-flex align-items-center justify-content-between sticky-top">
         <h3 class="mb-0 h-font">TNV WEBSITE</h3>
         <a href="logout.php" class="btn btn-light btn-sm">LOG OUT</a>
--- index.php
+++ index.php
@@ -541,41 +541,67 @@ magni tempora ea.
 </div>
 
 <!-- Reach Us -->
+
+<?php
+require ('admin/inc/db_config.php');
+$contact_q= "SELECT * FROM `contact_details` WHERE `sr_no`=?";
+$values = [1];
+$contact_r = mysqli_fetch_assoc(select($contact_q, $values, 'i'));
+
+
+?>
+
 <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">REACH US</h2>
+
 <div class="container">
   <div class="row">
     <div class="col-lg-8 col-md-8 p-4 mb-lg-0 mb-3 bg-white rounded">
-      <iframe class="w-100 rounded" height="320px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d67367.68287654064!2d8.03148905!3d58.1529776!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46380258d5607a5b%3A0xdf0c0d6fc81c58a4!2sKristiansand!5e0!3m2!1sno!2sno!4v1731414685342!5m2!1sno!2sno" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <iframe class="w-100 rounded" height="320px" src="<?php echo $contact_r['iframe'] ?>"></iframe>
     </div>
 
     <div class="col-lg-4 col-md-4">
       <div class="bg-white p-4 rounded mb-4">
         <h5>Call Us</h5>
-        <a href="tel:+4590033117" class="d-inline-block mb-2 text-decoration-none text-dark">
-          <i class="bi bi-telephone-fill"></i> +4590033117
+        <a href="tel:+<?php echo $contacts_r['pn1'] ?>" class="d-inline-block mb-2 text-decoration-none text-dark">
+          <i class="bi bi-telephone-fill"></i> <?php echo $contact_r['pn1'] ?>
         </a>
         <br>
-        <a href="tel:+4590033117" class="d-inline-block text-decoration-none text-dark">
-          <i class="bi bi-telephone-fill"></i> +4590033117
-        </a>
+        <?php 
+        if (isset($contact_r['pn2']) && trim($contact_r['pn2']) != '') {
+          echo <<<data
+          <a href="tel:+{$contact_r['pn2']}" class="d-inline-block mb-2 text-decoration-none text-dark">
+            <i class="bi bi-telephone-fill"></i> {$contact_r['pn2']}
+          </a>
+          data;
+      }
+      
+        ?>
+        
       </div>
 
       <!-- Follow Us Section -->
       <div class="bg-white p-4 rounded mb-4">
         <h5>Follow us</h5>
-        <a href="#" class="d-inline-block mb-3">
-          <span class="badge bg-light text-dark fs-6 p-2">
-            <i class="bi bi-twitter me-1"></i>Twitter
-          </span>
-        </a>
-        <br>
-        <a href="#" class="d-inline-block mb-3">
+        <?php
+        if($contact_r['tw']!=''){
+          echo<<<data
+          <a href="$contact_r[tw]" class="d-inline-block mb-3">
+              <span class="badge bg-light text-dark fs-6 p-2">
+                <i class="bi bi-twitter me-1"></i>Twitter
+              </span>
+            </a>
+            </br>
+          data;
+        }
+        ?>
+        
+        <a href="<?php echo $contact_r['fb']?>" class="d-inline-block mb-3">
           <span class="badge bg-light text-dark fs-6 p-2">
             <i class="bi bi-facebook me-1"></i>Facebook
           </span>
         </a>
         <br>
-        <a href="#" class="d-inline-block mb-3">
+        <a href="<?php echo $contact_r['insta']?>" class="d-inline-block mb-3">
           <span class="badge bg-light text-dark fs-6 p-2">
             <i class="bi bi-instagram me-1"></i>Instagram
           </span>
