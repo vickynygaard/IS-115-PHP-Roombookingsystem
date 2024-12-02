d447687b33198ca426b0551135e9d96de7861982
Victoria Nygård <112623372+vickynygaard@users.noreply.github.com>
2024-12-02 22:53:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -70,47 +70,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             <div class="d-flex">
 
-                <button type="button" class="btn btn-outline-dark shadow-none me-lg-2 me-2" data-bs-toggle="modal" data-bs-target="#loginModal">
-                    Login
-                </button>
-                <a class="btn btn-outline-dark shadow-none me-lg-2 me-2" href="http://localhost/MyWebsite/homepage/signup.php">Sign up</a>
+            <a class="btn btn-outline-dark shadow-none me-lg-2 me-2" href="http://localhost/MyWebsite/homepage/login.php">Login</a>
+            <a class="btn btn-outline-dark shadow-none me-lg-2 me-2" href="http://localhost/MyWebsite/homepage/signup.php">Sign up</a>
 
                 </div>
                 </div>
             </div>
         </div>
     </nav>
-
-  <!-- Modal -->
-  <div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <form>
-        <div class="modal-header">
-          <h5 class="modal-title d-flex align-items-center">
-          <i class="bi bi-person-circle fs-3 me-2 "></i>User Login
-    </h5>
-          <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-        <div class="mb-3">
-      <label class="form-label">Email address</label>
-      <input type="email" name="email" class="form-control shadow-none">
-        </div>
-        <div class="mb-4">
-      <label class="form-label">Password</label>
-      <input type="password" name="password" class="form-control shadow-none">
-        </div>
-      <div class="d-flex align-items-center justify-content-between mb-2">
-        <button type="submit" class="btn btn-dark shadow-none">LOGIN</button>
-        <a href="javascript: void(0)" class="text-secondary text-decoration-none">Forgot Password </a>
-      </div>
-
-        </div>
-        </form>
-      </div>
-    </div>
-  </div>
 
   <?php require ('html/swiper.php'); //Swiper section - pictures front page?>
 
