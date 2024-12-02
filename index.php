825c60435701484a60523ce28463d79239db1217
vickynygaard <vanygaard@uia.no>
2024-12-02 15:08:31 +0100

    m

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
+<?php
+require_once 'inc/input_helpers.php';
+require_once 'inc/config.php';
+
+session_start();
+
+//Initialize array where error messages are stored
+$errors = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+  $_SESSION['fields'] = [
+    'checkin' => clean_input('checkin', $_POST['checkin']),
+    'checkout' => clean_input('checkout', $_POST['checkout']),
+  ];
+
+  validate_past_date($_SESSION['fields'], $errors);
+
+    if (empty($errors)) {
+      //Redirect til rooms.php if successful validation
+      header("Location: homepage/rooms.php");
+      exit;
+    }
+    else {
+      echo "error";
+    }
+
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -65,11 +95,11 @@
         <div class="modal-body">
         <div class="mb-3">
       <label class="form-label">Email address</label>
-      <input type="email" class="form-control shadow-none">
+      <input type="email" name="email" class="form-control shadow-none">
         </div>
         <div class="mb-4">
       <label class="form-label">Password</label>
-      <input type="password" class="form-control shadow-none">
+      <input type="password" name="password" class="form-control shadow-none">
         </div>
       <div class="d-flex align-items-center justify-content-between mb-2">
         <button type="submit" class="btn btn-dark shadow-none">LOGIN</button>
@@ -89,34 +119,41 @@
     <div class="row">
       <div class="col-lg-12 bg-white shadow p-4 rounded">
         <h5 class="mb-4">Check Booking Availability</h5>
-        <form>
+        <form method="POST" action="">
           <div class="row align-items-end">
             <div class="col lg-3 mb-3">
               <label class="form-label" style="font-weight:500;">Check-in</label>
-              <input type="date" class="form-control shadow-none">
+              <input type="date" name="checkin" class="form-control shadow-none">
+                <?php if (isset($errors['checkin'])): ?>
+                <span class="error"><?= $errors['checkin'] ?></span><?php endif; ?>
             </div>
             <div class="col lg-3 mb-3">
               <label class="form-label" style="font-weight:500;">Check-out</label>
-              <input type="date" class="form-control shadow-none">
+              <input type="date" name="checkout" class="form-control shadow-none">
+                <?php if (isset($errors['checkout'])): ?>
+                <span class="error"><?= $errors['checkout'] ?></span><?php endif; ?>
             </div>
             <div class="col-lg-3 mb-3">
-              <label class="form-label" style="font-weight:500;">Adult</label>
-              <select class="form-select shadow-none">
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
+              <label class="form-label" style="font-weight:500;">Adults</label>
+              <select name="adults" class="form-select shadow-none">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
             </select>
             </div>
             <div class="col-lg-2 mb-3">
               <label class="form-label" style="font-weight:500;">Children</label>
-              <select class="form-select shadow-none">
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
+              <select name="children" class="form-select shadow-none">
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
             </select>
             </div>
             <div class="col-lg-1 mb-lg-3 mt-2">
-              <button type="submit" class="btn text-white shadow-none custom-bg">Submit</button>
+              <button type="submit" class="btn text-white shadow-none custom-bg">Search</button>
             </div>
           </div>
         </form>
